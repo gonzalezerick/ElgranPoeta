a3aa877ea549e2e20ee3ae4042dc2aafc70c4fb9
gonzalezerick <139947688+gonzalezerick@users.noreply.github.com>
2024-06-24 15:47:04 -0400

    se modificó productos.py

--- productos.py
+++ productos.py
@@ -16,6 +16,17 @@ def crear_producto(tipo, nombre, editorial, autores, descripcion):
 def papelera_productos():
     print("\n--- Papelera de Productos ---")
 
+def eliminar_producto(id_producto):
+    conn = connect()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM productos WHERE id = %s', (id_producto,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print(f"Producto con ID '{id_producto}' eliminado.")
+    else:
+        print("No se pudo conectar a la base de datos.")
 
 def mover_a_papelera_producto(id_producto):
     conn = connect()
@@ -45,18 +56,19 @@ def listar_productos_en_bodega():
     conn = connect()
     if conn is not None:
         cursor = conn.cursor()
-        cursor.execute('SELECT tipo, nombre, descripcion, codigo_producto FROM productos WHERE eliminado = 0')
+        cursor.execute('SELECT tipo, nombre, descripcion FROM productos WHERE eliminado = 0')
         productos = cursor.fetchall()
         if productos:
             print("\n--- Productos Disponibles en la Bodega ---")
-            for tipo, nombre, descripcion, codigo in productos:
-                print(f"Tipo: {tipo}, Nombre: {nombre}, Descripción: {descripcion}, Código: {codigo}")
+            print("+" + "-" * 80 + "+")
+            print("| {:<10} | {:<20} | {:<30} |".format("Tipo", "Nombre", "Descripción"))
+            print("+" + "-" * 80 + "+")
+            for tipo, nombre, descripcion in productos:
+                print("| {:<10} | {:<20} | {:<30} |".format(tipo, nombre, descripcion))
+            print("+" + "-" * 80 + "+")
         else:
             print("No hay productos disponibles en la bodega.")
         cursor.close()
         conn.close()
     else:
         print("No se pudo conectar a la base de datos.")
-
-if __name__ == "__main__":
-    listar_productos_en_bodega()
